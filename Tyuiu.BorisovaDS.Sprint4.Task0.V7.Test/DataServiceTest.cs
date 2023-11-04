e1bea84b7d272f84ca1a175be78ea37d7718823b
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-11-04 17:49:54 +0300

    Add project files.

---
+++ Tyuiu.BorisovaDS.Sprint4.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BorisovaDS.Sprint4.Task0.V7.Lib;
+
+namespace Tyuiu.BorisovaDS.Sprint4.Task0.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultOddArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 9, 8, 7, 9, 5, 4, 3, 2, 3, 7 };
+            int res = ds.GetMultOddArrEl(array);
+            int result = 178605;
+            Assert.AreEqual(result, res);
+        }
+    }
+}
---
+++ Tyuiu.BorisovaDS.Sprint4.Task0.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BorisovaDS.Sprint4.Task0.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BorisovaDS.Sprint4.Task0.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("228e66b4-0b9b-40ab-b43e-e1438aa1a4d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
